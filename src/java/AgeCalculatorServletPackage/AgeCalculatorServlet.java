db33a7f69d8d15b0e1469de23b2045ebcd46fdae
843827 <843827@SAIT224924IT.ACDM.DS.SAIT.CA>
2020-09-23 17:36:07 -0600

    delete some useless part

--- src/java/AgeCalculatorServletPackage/AgeCalculatorServlet.java
+++ src/java/AgeCalculatorServletPackage/AgeCalculatorServlet.java
@@ -52,9 +52,5 @@ public class AgeCalculatorServlet extends HttpServlet {
     } 
 
 
-    @Override
-    public String getServletInfo() {
-        return "Short description";
-    }// </editor-fold>
 
 }
