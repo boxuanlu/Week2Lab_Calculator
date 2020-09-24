7b266ac24d5764356b4d4f6d29a60cd540dd1c86
843827 <843827@SAIT224924IT.ACDM.DS.SAIT.CA>
2020-09-23 18:31:13 -0600

    finish lab

--- src/java/arithmeticcalculatorPackage/ArithmeticCalculatorServlet.java
+++ src/java/arithmeticcalculatorPackage/ArithmeticCalculatorServlet.java
@@ -12,44 +12,75 @@ import javax.servlet.http.HttpServletResponse;
 public class ArithmeticCalculatorServlet extends HttpServlet {
 
 
-
-    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
-    /**
-     * Handles the HTTP <code>GET</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
+            String message = "-----";
+            request.setAttribute("message", message);
         getServletContext().getRequestDispatcher("/WEB-INF/arithmeticcalculator.jsp").forward(request, response);
     }
 
-    /**
-     * Handles the HTTP <code>POST</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        
-    }
+        String first = request.getParameter("first");
+        String second = request.getParameter("second");
+        String cal = request.getParameter("button");
+        if(first.equals("")||second.equals("")){
+            String message = "input both number";
+            request.setAttribute("message", message);
+            request.setAttribute("first", first);
+            request.setAttribute("second", second);
+            getServletContext().getRequestDispatcher("/WEB-INF/arithmeticcalculator.jsp").forward(request, response);
+        }else if(isNumber(first)&isNumber(second)){
+            int firstinput = Integer.parseInt(first);
+            int secondinput = Integer.parseInt(second);
+            if(cal.equals("+")){
+                int total = firstinput + secondinput;
+                String message = " " + total;
+                request.setAttribute("message", message);
+                request.setAttribute("first", first);
+                request.setAttribute("second", second);
+                getServletContext().getRequestDispatcher("/WEB-INF/arithmeticcalculator.jsp").forward(request, response);
+            }else if(cal.equals("-")){
+                int total = firstinput - secondinput;
+                String message = " " + total;
+                request.setAttribute("message", message);
+                getServletContext().getRequestDispatcher("/WEB-INF/arithmeticcalculator.jsp").forward(request, response);
+            }else if(cal.equals("*")){
+                int total = firstinput * secondinput;
+                String message = " " + total;
+                request.setAttribute("message", message);
+                request.setAttribute("first", first);
+                request.setAttribute("second", second);
+                getServletContext().getRequestDispatcher("/WEB-INF/arithmeticcalculator.jsp").forward(request, response);
+            }else if(cal.equals("%")){
+                int total = firstinput / secondinput;
+                String message = " " + total;
+                request.setAttribute("message", message);
+                request.setAttribute("first", first);
+                request.setAttribute("second", second);
+                getServletContext().getRequestDispatcher("/WEB-INF/arithmeticcalculator.jsp").forward(request, response);
+            }
+            
+        }else {
+            String message = "invalid input";
+            request.setAttribute("message", message);
+            getServletContext().getRequestDispatcher("/WEB-INF/arithmeticcalculator.jsp").forward(request, response);
+        }
 
-    /**
-     * Returns a short description of the servlet.
-     *
-     * @return a String containing servlet description
-     */
-    @Override
-    public String getServletInfo() {
-        return "Short description";
-    }// </editor-fold>
+    }
+    
+    
+        public static boolean isNumber(String s) 
+    { 
+        for (int i = 0; i < s.length(); i++) 
+        if (Character.isDigit(s.charAt(i))  
+            == false) 
+            return false; 
+  
+        return true; 
+    } 
+
 
 }
